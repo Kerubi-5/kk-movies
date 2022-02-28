374d9b7dfb738b1d258703a1e211f26306c70cd1
John Kim A. Querobines <querobinesj@gmail.com>
2022-02-28 23:18:13 +0800

    adds animation

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import Movie from "../components/Movie";
 import { fetchApi } from "../state/movieReducer";
 import Loading from "./Loading";
@@ -24,8 +24,11 @@ const Movies = () => {
       {loading ? (
         <Loading />
       ) : (
-        <motion.section className="overflow-hidden container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 px-4 gap-5 text-gray-700 max-w-screen-lg mx-auto pb-5">
-          {renderMovies()}
+        <motion.section
+          layout
+          className="overflow-hidden container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 px-4 gap-5 text-gray-700 max-w-screen-lg mx-auto pb-5"
+        >
+          <AnimatePresence>{renderMovies()}</AnimatePresence>
         </motion.section>
       )}
     </>
