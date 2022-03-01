7a15dad984625d3b3c9ba01f1e585f83e450e9d9
John Kim A. Querobines <querobinesj@gmail.com>
2022-03-01 19:16:20 +0800

    implements hover

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = () => {
+  return (
+    <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold px-4 border border-gray-400 rounded  h-10 self-center">
+      Learn More
+    </button>
+  );
+};
+
+export default Button;
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import Button from "./Button";
 const Movie = ({ movie }) => {
   return (
     <motion.div
@@ -6,13 +7,20 @@ const Movie = ({ movie }) => {
       animate={{ opacity: 1, scale: 1 }}
       initial={{ opacity: 0, scale: 0 }}
       exit={{ opacity: 1, scale: 0 }}
+      className="group"
     >
       <h1 className="font-bold tracking-wide truncate">{movie.title}</h1>
-      <img
-        alt="gallery"
-        className="rounded-lg w-full"
-        src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-      />
+      <div className="relative">
+        <img
+          alt="gallery"
+          className="rounded-lg w-full"
+          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+        />
+        <div className="absolute opacity-0 group-hover:opacity-100 bg-black/75 top-0 bottom-0 left-0 right-0 flex flex-col gap-5 align-middle justify-center cursor-pointer duration-300 rounded-lg">
+          <Button />
+          <Button />
+        </div>
+      </div>
     </motion.div>
   );
 };
