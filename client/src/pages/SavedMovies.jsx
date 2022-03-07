35f5f8d4ce271e10bb6235373a2ccb119858b15f
John Kim A. Querobines <querobinesj@gmail.com>
2022-03-07 22:25:14 +0800

    displays saved movies

--- client/src/pages/SavedMovies.jsx
+++ client/src/pages/SavedMovies.jsx
@@ -1,30 +1,41 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect, useState } from "react";
+import Loading from "./Loading";
 const SavedMovies = () => {
+  const [savedMovies, setSavedMovies] = useState();
   const { getAccessTokenSilently, user } = useAuth0();
 
-  const publicRoute = async () => {
-    const accessToken = await getAccessTokenSilently();
-
-    fetch(`http://localhost:5000/saved?email=${user.email}`, {
-      method: "GET",
-      headers: new Headers({
-        "Content-Type": "application/json; charset=UTF-8",
-        Authorization: `Bearer ${accessToken}`,
-      }),
-    })
-      .then((data) => data.json())
-      .then((resp) => console.log(resp));
+  const renderList = () => {
+    return savedMovies.map((item) => {
+      return (
+        <div>
+          Name: {item.name} imgSrc: {item.imgSrc} <br />
+        </div>
+      );
+    });
   };
+  useEffect(() => {
+    const fetchSavedMoviesApi = async () => {
+      const accessToken = await getAccessTokenSilently();
 
-  return (
-    <>
-      <h1>Saved Movies</h1>
+      fetch(`http://localhost:5000/saved?email=${user?.email}`, {
+        method: "GET",
+        headers: new Headers({
+          "Content-Type": "application/json; charset=UTF-8",
+          Authorization: `Bearer ${accessToken}`,
+        }),
+      })
+        .then((data) => data.json())
+        .then((resp) => {
+          const [x] = resp.results;
+          setSavedMovies(x.movies);
+        });
+    };
 
-      <button className="p-4 border-2" onClick={publicRoute}>
-        Public
-      </button>
-    </>
-  );
+    fetchSavedMoviesApi();
+  }, [getAccessTokenSilently, user?.email]);
+
+  return <>{user ? renderList() : <Loading />}</>;
 };
 
 export default SavedMovies;
