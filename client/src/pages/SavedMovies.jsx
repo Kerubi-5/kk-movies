17cf6153c3701440e0c34e3c4269cff6946144e7
John Kim A. Querobines <querobinesj@gmail.com>
2022-03-08 19:04:09 +0800

    adds a design for the saved movies

--- client/src/pages/SavedMovies.jsx
+++ client/src/pages/SavedMovies.jsx
@@ -3,17 +3,23 @@ import { useEffect, useState } from "react";
 import Loading from "./Loading";
 const SavedMovies = () => {
   const [savedMovies, setSavedMovies] = useState();
-  const { getAccessTokenSilently, user } = useAuth0();
+  const { getAccessTokenSilently, user, isLoading } = useAuth0();
 
   const renderList = () => {
-    return savedMovies.map((item) => {
+    return savedMovies?.map((item) => {
       return (
-        <div>
-          Name: {item.name} imgSrc: {item.imgSrc} <br />
+        <div key={item.id} className="rounded-lg">
+          <img
+            className="rounded-lg"
+            src={`https://image.tmdb.org/t/p/w500/${item.imgSrc}`}
+            alt={"fave movies"}
+          />
+          <p className="text-sm capitalize text-slate-500">{item.name}</p>
         </div>
       );
     });
   };
+
   useEffect(() => {
     const fetchSavedMoviesApi = async () => {
       const accessToken = await getAccessTokenSilently();
@@ -27,15 +33,38 @@ const SavedMovies = () => {
       })
         .then((data) => data.json())
         .then((resp) => {
-          const [x] = resp.results;
-          setSavedMovies(x.movies);
+          const [x] = resp?.results ?? [];
+          console.log(x);
+
+          setSavedMovies(x?.movies);
         });
     };
 
-    fetchSavedMoviesApi();
-  }, [getAccessTokenSilently, user?.email]);
+    if (!isLoading) fetchSavedMoviesApi();
+  }, [getAccessTokenSilently, isLoading, user]);
 
-  return <>{user ? renderList() : <Loading />}</>;
+  if (isLoading) return <Loading />;
+
+  return (
+    <>
+      {user ? (
+        <div className="border rounded-lg shadow-lg m-2 p-5">
+          <div className="grid place-content-center text-center mb-5">
+            <img className="rounded-full" src={user?.picture} alt="Profile" />
+            <h4 className="">{user?.nickname}</h4>
+          </div>
+          <div className="text-center">
+            <h2 className="font-semibold">Favorite Movies</h2>
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5">
+              {renderList()}
+            </div>
+          </div>
+        </div>
+      ) : (
+        "no user"
+      )}
+    </>
+  );
 };
 
 export default SavedMovies;
