4dcce242d4bea0126e93c080b7573ad6572e1281
John Kim A. Querobines <querobinesj@gmail.com>
2022-03-08 20:56:08 +0800

    for heroku

--- client/src/state/index.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import movieReducer from "./movieReducer";
-export const store = configureStore({
-  reducer: {
-    movies: movieReducer,
-  },
-});
