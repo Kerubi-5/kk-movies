d9acc4a512315dc2940cec16d74e5fa4473d71d8
John Kim A. Querobines <querobinesj@gmail.com>
2022-03-08 19:18:58 +0800

    cleans up conn

--- server/config/db.js
+++ server/config/db.js
@@ -3,12 +3,15 @@ const mongoose = require("mongoose");
 const user = process.env.MONGO_USER;
 const pass = process.env.MONGO_PASS;
 const db = process.env.MONGO_DB;
+const uri = `mongodb+srv://${user}:${pass}@kk.hqg1x.mongodb.net/${db}?retryWrites=true&w=majority`;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
 const conn = () => {
   mongoose
-    .connect(
-      `mongodb+srv://${user}:${pass}@kk.hqg1x.mongodb.net/${db}?retryWrites=true&w=majority`
-    )
+    .connect(uri, options)
     .then(() => console.log("Mongo Atlas DB Connection successful"))
     .catch((err) => console.log(`Error MongoDB Connection: ${err}`));
 };
